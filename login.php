a26b2627c63a1784f597d3c422b07010357b0b6f
Marcelo Ribeiro da Silva <marceloskype@gmail.com>
2019-04-14 14:14:12 -0300

    comit do arquivo login e demais arquivos vazios

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Login Com o Google API< + Boostrap 4</title>
+</head>
+<body>
+        <div class="container" style="margin-top: 100px" >
+            <div class="row justify-center">
+                <div class="col-md-6 col-offset-3" align="center">
+                    <img src="logo.png"><br><br>
+                    <form  >
+                        <input type="email" name="email" class="form-control" placeholder="Email.." id="email"><br>
+                       <input type="password" class="form-control" name="senha" placeholder="Senha..." id="email"><br>
+                       <input type="submit"  value="Log in" class="btn btn-primary">
+                       <input type="button" value="Login com Google" class="btn btn-danger">
+                    </form>
+
+                </div>
+            </div>
+        </div>
+</body>
+</html>
